deefe9dfe36590e130dc564e55797a971d04a779
arunkumar <allamarunkumar@gmail.com>
2026-09-01 09:11:46 +0530

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;  // Predefined Namespaces  or .dll(dynamic link lib) or public Assemblies
+
+namespace ConsoleApp  // Userdefined Namespace  or Application
+{
+    internal class Program  // class declaration
+    {
+        static void Main(string[] args)  // Method
+        {
+            // Document Area or Coding Area
+        }
+    }
+}
+// single line comment
+
+
+/*
+ * MultiLine Comment
+ */
+
+/*
+ * Comments can't be execucted , for  user reference
+ */
+
+///
+/// dfsafdas
+/// sfsaf
+/// asfasf
+/// fsdd
+/// fsdf
+/// safsaf
+/// dafsfdsf
+/// 
+////
